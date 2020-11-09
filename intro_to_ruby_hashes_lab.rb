a92e8938fb39ae56a56d0da710bb45e75eaf0710
Elias Campos <ecampose76@gmail.com>
2020-11-09 23:35:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,7 @@ base_hash = {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
-      :four_pieces_owened => 200
+      :four_pieces_owned => 200
     },
     :names => {}
   }
